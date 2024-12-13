ddba6a7bc6e4d6c4cb3a49e83ee9a6ca8c26a1ad
Edwin1821 <vsedwinsamuel2004@gmail.com>
2024-12-13 18:48:08 +0530

    base updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import './App.css'
-import FormSubmit from './components/FormSubmit';
-import RootLayout from './components/layout/RootLayout';
-import ReadForm from './components/ReadForm';
-import UpdateForm from './components/UpdateForm';
-
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import FormSubmit from "./components/FormSubmit";
+import RootLayout from "./components/layout/RootLayout";
+import ReadForm from "./components/ReadForm";
+import UpdateForm from "./components/UpdateForm";
 
 const Routes = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/viteproject' element={<RootLayout/>}>
-       <Route path='create' element={<FormSubmit/>}/>
-       <Route path='read' element={<ReadForm/>}/>
-       <Route path='/update/:id' element={<UpdateForm/>}/>
+    <Route path="/viteproject" element={<RootLayout />}>
+      <Route path="create" element={<FormSubmit />} />
+      <Route path="read" element={<ReadForm />} />
+      <Route path="/update/:id" element={<UpdateForm />} />
     </Route>
   )
-)
+);
 
 function App() {
   return (
     <>
-      <RouterProvider router={Routes}/>
+      <RouterProvider router={Routes} />
     </>
-  )
+  );
 }
 
 export default App;
